5fab28f422277239437bd8cb2862fcbbac60475c
Yuvaraj-96 <yuvarajmechmailam@gmail.com>
2022-04-26 00:15:50 +0530

    Frontend code commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React,{useState}from 'react';
 import './Login.css';
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email:"",password:""
+  })
+  const handelChange=(e)=>{
+    const { name, value} = e.traget;
+    setUser({
+      ...user,[name]: value
+    })
+  }
   return (
    <div className="login">
+     {console.log(("User : "+user))}
      <h1>Login</h1>
-     <input type='text' placeholder='Enter your Email'></input>
-     <input type='password' placeholder='Enter your Password'></input>
+     <input name='email' value={user.email} type='text' placeholder='Enter your Email' onChange={handelChange}></input>
+     <input name='password' value={user.password} type='password' placeholder='Enter your Password' onChange={handelChange}></input>
      <div className="button">Login</div>
      <div>Or</div>
      <div className="button">Register</div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Register.css';
 
 const Register = () => {
+  const [user, setUser] = useState({
+    name:"",email:"",password:"",reEnterPassword:""
+  })
+  const handelChange=(e)=>{
+    const { name, value} = e.traget;
+    setUser({
+      ...user,[name]: value
+    })
+
+  }
   return (
     <div className='register'>
     <h1>Register</h1>
-    <input type='text' placeholder='Enter your Name'></input>
-    <input type='text' placeholder='Enter your Email'></input>
-    <input type='password' placeholder='Enter your Email'></input>
-    <input type='password' placeholder='Re-enter your Password'></input>
+    <input name='name' value={user.name} type='text' placeholder='Enter your Name' onChange={handelChange}></input>
+    <input name='email' value={user.email}  type='text' placeholder='Enter your Email' onChange={handelChange}></input>
+    <input name='password' value={user.password}  type='password' placeholder='Enter your Email'onChange={handelChange}></input>
+    <input name='reEnterPassword' value={user.reEnterPassword}  type='password' placeholder='Re-enter your Password' onChange={handelChange}></input>
     <div className="button">Register</div>
     <div>Or</div>
     <div className="button">Login</div>
